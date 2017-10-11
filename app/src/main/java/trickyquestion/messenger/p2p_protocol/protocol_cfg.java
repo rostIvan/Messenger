f9641ff05f952f0fe30ccc8d0621961827eadd67
Zen <olesy159357@gmail.com>
2017-10-11 19:13:03 +0300

    Add class for building broadcast message, service send this msg every send timeout

---
+++ app/src/main/java/trickyquestion/messenger/p2p_protocol/protocol_cfg.java
@@ -0,0 +1,10 @@
+package trickyquestion.messenger.p2p_protocol;
+
+/**
+ * Created by Zen on 11.10.2017.
+ */
+
+public class protocol_cfg {
+    static int HEARTBEAT_TTL = 5000;
+    static int HEARTBEAT_DELAY = 2500;
+}
