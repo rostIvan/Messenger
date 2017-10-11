a190fec3c47bdf775c93ce9725cd43882bda1765
Zen <olesy159357@gmail.com>
2017-10-11 19:09:01 +0300

    Add class for building broadcast message, service send this msg every send timeout

---
+++ app/src/main/java/trickyquestion/messenger/p2p_protocol/build_broadcast_protocol_msg.java
@@ -0,0 +1,37 @@
+package trickyquestion.messenger.p2p_protocol;
+
+/**
+ * Created by Zen on 11.10.2017.
+ */
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.UUID;
+
+/**
+ * Created by Zen on 23.09.2017.
+ */
+
+public class build_broadcast_protocol_msg {
+    /**
+     * Create heartbeat packet data
+     * @param UserName also know as user Login
+     * @param UserID user id which identifies user in network
+     * @param IP client smart phone/tablet IP address
+     * @return return string value which represent protocol heartbeat packet(size 109)
+     */
+    static public String genHeartbeatPacket(String UserName, UUID UserID, String IP){
+        StringBuilder UsrNamePart = new StringBuilder("$$$$$$$$$$$$$$$$$$$");
+        UsrNamePart.replace(0,UserName.length(),UserName);
+        StringBuilder UserIDPart = new StringBuilder(UserID.toString());
+        StringBuilder UserIPPart = new StringBuilder("$$$$$$$$$$$$$$$");
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH-mm-ss", Locale.US);
+        String SendTime = sdf.format(Calendar.getInstance().getTime());
+        UserIPPart.replace(0,IP.length(),IP);
+        return new String
+                ("P2P_HEARTBEAT:" + UsrNamePart + ":" + UserIDPart + ":" +UserIPPart+ ":" + SendTime + ":P2P_HEARTBEAT");
+    }
+}
