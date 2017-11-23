eb0ef8e4e4b238fcff180137d641d929b633af0f
rost <bbwgd77@gmail.com>
2017-11-23 19:35:01 +0200

    add scrolling nick and id with menu items in settings

--- app/src/main/java/trickyquestion/messenger/settings_screen/view/SettingActivity.java
+++ app/src/main/java/trickyquestion/messenger/settings_screen/view/SettingActivity.java
@@ -1,22 +1,25 @@
 package trickyquestion.messenger.settings_screen.view;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bignerdranch.expandablerecyclerview.Model.ParentObject;
 import com.flask.colorpicker.ColorPickerView;
-import com.flask.colorpicker.OnColorChangedListener;
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 import java.util.List;
@@ -40,11 +43,16 @@ public class SettingActivity extends AppCompatActivity implements ISettingView {
     TextView login;
     @BindView(R.id.setting_id)
     TextView id;
+    @BindView(R.id.rv_setting_expanded)
+    RecyclerView recyclerView;
 
-    private RecyclerView recyclerView;
     private ExpandableAdapter adapter;
-    private ISettingPresenter presenter;
+    private AlertDialog colorPicker;
+
     private ThemePreference themePreference;
+    private ISettingPresenter presenter;
+
+    private static final String EXTRA_COLOR_PICKER_DIALOG_STATE = "save color picker dialog state";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -54,12 +62,6 @@ public class SettingActivity extends AppCompatActivity implements ISettingView {
         if (presenter == null) presenter = new SettingPresenter(this);
         themePreference = new ThemePreference(this);
         presenter.onCreate();
-    }
-
-    @Override
-    public void setUserData() {
-        login.setText(presenter.getUserName());
-        id.setText(presenter.getUserId());
     }
 
     @Override
@@ -81,6 +83,12 @@ public class SettingActivity extends AppCompatActivity implements ISettingView {
     }
 
     @Override
+    public void setUserData() {
+        login.setText(presenter.getUserName());
+        id.setText(presenter.getUserId());
+    }
+
+    @Override
     public void customizeTheme() {
         toolbar.setBackgroundColor(themePreference.getPrimaryColor());
     }
@@ -95,13 +103,18 @@ public class SettingActivity extends AppCompatActivity implements ISettingView {
 
     @Override
     public void customizeRecycler() {
-        recyclerView = (RecyclerView) findViewById(R.id.rv_setting_expanded);
         adapter = new ExpandableAdapter(presenter, getParents());
         adapter.setCustomParentAnimationViewId(R.id.parent_list_item_expand_arrow);
         adapter.setParentClickableViewAnimationDefaultDuration();
         adapter.setParentAndIconExpandOnClick(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (colorPicker != null && colorPicker.isShowing()) colorPicker.dismiss();
     }
 
     @Override
@@ -149,27 +162,31 @@ public class SettingActivity extends AppCompatActivity implements ISettingView {
 
     @Override
     public void showColorPicker() {
-        final AlertDialog picker = ColorPickerDialogBuilder
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+            colorPicker = getColorPickerDialog(9, true);
+        else
+            colorPicker = getColorPickerDialog(6, false);
+        colorPicker.show();
+        colorPicker.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(themePreference.getPrimaryColor());
+        colorPicker.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(themePreference.getPrimaryColor());
+    }
+
+    private AlertDialog getColorPickerDialog(int density, boolean showPreview) {
+        return ColorPickerDialogBuilder
                 .with(this)
                 .setTitle("Choose color")
-                .initialColor(themePreference.getPrimaryColor())
+                .initialColor(getResources().getColor(R.color.colorWhite))
                 .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
-                .density(9)
+                .density(density)
                 .setPositiveButton("ok", (dialog, selectedColor, allColors) -> {
                     presenter.setThemePrimaryColor("#" + Integer.toHexString(selectedColor));
                 })
-                .setNegativeButton("cancel", (dialog, which) -> {
-                    dialog.dismiss();
-                })
-                .showColorEdit(true)
-                .showAlphaSlider(true)
-                .showColorPreview(true)
-                .showLightnessSlider(true)
+                .setNegativeButton("cancel", (dialog, which) -> dialog.dismiss())
+                .showColorEdit(false)
+                .noSliders()
+                .showColorPreview(showPreview)
                 .setColorEditTextColor(Color.BLACK)
                 .build();
-        picker.show();
-        picker.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(themePreference.getPrimaryColor());
-        picker.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(themePreference.getPrimaryColor());
     }
 
     private List<ParentObject> getParents() {
