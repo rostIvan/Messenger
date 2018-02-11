a3b4f2d4af620b00b7008d4c791da89bafd5c26d
rost <bbwgd77@gmail.com>
2018-02-11 20:43:15 +0200

    test jenkins

---
+++ app/src/test/java/trickyquestion/messenger/unit/JenkinsTest.java
@@ -0,0 +1,15 @@
+package trickyquestion.messenger.unit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class JenkinsTest {
+    @Test
+    public void test() {
+        assertEquals(2 * 2, 4);
+    }
+}
